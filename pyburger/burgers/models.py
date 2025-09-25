4f6ca3f3011fc8f97b8376804695c3efb77f8bb0
freemanyoo <freemanyoo@gmail.com>
2025-09-25 14:59:17 +0900

    250925-2

--- pyburger/burgers/models.py
+++ pyburger/burgers/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-
-
 class Burger(models.Model):
     name = models.CharField(max_length=100)
     price =models.IntegerField(default=0)
--- pyburger/pyburger/views.py
+++ pyburger/pyburger/views.py
@@ -9,8 +9,27 @@ def main(request):
     return render(request, 'main.html')
 
 def burger_list(request):
-    # return HttpResponse("파이버거의 햄버거 목록입니다")
+    # return HttpResponse("파이버거의 햄버거 목록입니다.~~~")
     burgers = Burger.objects.all()
     print("전체 햄버거 목록:", burgers)
     context = {'burgers': burgers}
     return render(request, 'burger_list.html', context)
+
+def burger_search(request):
+    # 1. 화면에서 , 검색어 받아와서,
+    # 2. 디비에서 검색하고,
+    # 3. 검색 결과를 화면에 전달하고,
+    # search/?keyword=더블
+    keyword = request.GET.get("keyword")
+    if keyword is not None:
+        burgers = Burger.objects.filter(name__contains=keyword)
+        print("검색결과: ", burgers)
+    else:
+        burgers = Burger.objects.none()
+
+    # 디비에서 조회
+
+    print("검색 결과: ", burgers)
+    # 조회된 결과 데이터를 화면에 전달하기.
+    context = {'burgers': burgers}
+    return render(request, "burger_search.html",context)
